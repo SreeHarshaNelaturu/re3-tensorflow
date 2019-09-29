beae02f4ce8119614d1a6a5e6780059249015d3f
SreeHarshaNelaturu <sreeharsha_murali@srmuniv.edu.in>
2019-09-29 18:22:17 +0530

    updated runway model

--- runway_model.py
+++ runway_model.py
@@ -1,10 +1,3 @@
-import cv2
-import argparse
-import glob
-import numpy as np
-import os
-import time
-import sys
 import runway
 from runway.data_types import *
 basedir = os.path.dirname(__file__)
@@ -23,38 +16,66 @@ def setup(opts):
 
 inputs = {"input_image" : image}
 outputs = {"op_bbox" : array(image_bounding_box)}
+#global counter
+
+
 
 @runway.command("track_object", inputs = inputs, outputs = outputs, description="Track Selected Object")
 def track_object(tracker, inputs):
-    img = np.array(inputs["input_image"])
-    init_bbox = [160, 143, 460, 375]
-    #init_bbox = [0.491131,0.343612, 0.667406,0.505140 ]
-    global initialize, outputBoxToDraw
-    #initialize = True
+    global initialize
     initialize = True
-    if initialize:
-        boxToDraw = [init_bbox[0]/img.shape[0], init_bbox[1]/img.shape[1], init_bbox[2]/img.shape[0], init_bbox[3]/img.shape[1]]
-        print("This should run only once")
-        #boxToDraw = init_bbox
-        outputBoxToDraw = tracker.track('webcam', img[:, :, ::-1], boxToDraw)
-        initialize = False
 
-    print("Tracking")
-    outputBoxToDraw = tracker.track('webcam', img[:,:,::-1])
+    img = np.array(inputs["input_image"])
+    init_bbox = [0.491131,0.343612, 0.667406,0.505140 ]
+
+    print("This should run only once")
+    if initialize:
+       #Initally for the tracker to start, requires input bounding box on Frame #1
+       print("Object Tracker Intialized")
+       boxToDraw = init_bbox
+       outputBoxToDraw = tracker.track('webcam', img[:, :, ::-1], boxToDraw)
+       initialize = False #This does not work and needs to be looked into
+    else:
+        """The else condition is never going to actually be executed, 
+           as it isn't met at any time but the objective is to be 
+           able to run the tracker in prediction mode after frame 0"""
+        outputBoxToDraw = tracker.track('webcam', img[:, :, ::-1])
 
     outputBoxToDraw = [outputBoxToDraw]
-
+    print(outputBoxToDraw)
     return {"op_bbox" : outputBoxToDraw}
 
-    # normalize_bbox = [[outputBoxToDraw[0]/img.shape[0], outputBoxToDraw[1]/img.shape[1], outputBoxToDraw[2]/img.shape[0], outputBoxToDraw[3]/img.shape[1]]]
-    # print("nomralize", normalize_bbox)
-    # print(type(normalize_bbox[0]))
-    #return {"op_bbox" : normalize_bbox}
 if __name__ == "__main__":
     runway.run(model_options={"checkpoint" : "logs/checkpoints/"})
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" Unnormalized Data Methods, can be removed once the final I/O format is decided.
+        #init_bbox = [160, 143, 460, 375]
+        #boxToDraw = [init_bbox[0]/img.shape[0], init_bbox[1]/img.shape[1], init_bbox[2]/img.shape[0], init_bbox[3]/img.shape[1]]
+        # normalize_bbox = [[outputBoxToDraw[0]/img.shape[0], outputBoxToDraw[1]/img.shape[1], outputBoxToDraw[2]/img.shape[0], outputBoxToDraw[3]/img.shape[1]]]
+        # print("nomralize", normalize_bbox)
+        # print(type(normalize_bbox[0]))
+        #return {"op_bbox" : normalize_bbox}
+    """
 
 
